9e7991d422dfe718617c86dff73f9e703c0263a4
Rupesh Yadav <rupeshyadav8178@gmail.com>
2022-06-10 19:04:52 +0530

    msg

--- core/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-from . import views
-
-urlpatterns=[
-    path('',views.lobby,name="Lobby"),
-    path('room/',views.room,name='room')
-
-]
--- core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def lobby(request):
-    return render(request,'core/lobby.html')
-
-def room(request):
-    return render(request,'core/room.html')
